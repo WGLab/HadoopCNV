f8efa43ea63589ff5ebad2b921ee24210968a00a
Max He <maxy@dws2.(none)>
2015-07-26 22:36:20 -0500

    changed pom.xml and example/run.sh

--- example/run.sh
+++ example/run.sh
@@ -2,4 +2,4 @@
 
 hadoop jar ../target/penncnv-seq-0.1.jar edu.usc.PennCnvSeq -libjars $HADOOP_CLASSPATH config.txt
 #hadoop jar ../target/penncnv-seq-0.1.jar edu.usc.PennCnvSeq -libjars $HADOOP_BAM/target/hadoop-bam-7.0.1-SNAPSHOT-jar-with-dependencies.jar config.txt
-hdfs dfs -getmerge workdir/cnv/* results.txt
+hdfs dfs -getmerge workdir/cnv/$BAM_FILENAME/* results.txt
